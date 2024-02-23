88a1d0e3cc4ea0189658e1af121284ae4db2bf8b
arjun <arjun.vmaniyara@gmail.com>
2024-02-23 15:04:24 +0530

    config bug

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -5,7 +5,7 @@ export default {
 
     port: process.env.PORT,
     mongo: {
-        uri: process.env.MONGODB_HOST
+        uri: "mongodb+srv://cloud:I6WmTqosSvleUe0i@cluster0.4ww5r6f.mongodb.net/Top_Companies"
     },
     ACESS_TOKEN_SCERET: process.env.ACESS_TOKEN_SCERET,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
