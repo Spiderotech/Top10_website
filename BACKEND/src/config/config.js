585b892ad97d1c42c7cbb4e8ddccad7c6893a3d0
arjun <arjun.vmaniyara@gmail.com>
2024-03-07 15:35:02 +0530

    env error fix

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -7,10 +7,11 @@ export default {
     mongo: {
         uri: "mongodb+srv://cloud:I6WmTqosSvleUe0i@cluster0.4ww5r6f.mongodb.net/Top_Companies"
     },
-    ACESS_TOKEN_SCERET: process.env.ACESS_TOKEN_SCERET,
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-    S3_ACESS_KEY: process.env.S3_ACESS_KEY,
-    S3_SECRET_KEY: process.env.S3_SECRET_KEY,
+    ACESS_TOKEN_SCERET:"0ebe9d810658b83d5735b0e3daa8164f23911f5da8a0e4a1ee4ba105d5151a640edffe2592c333ffbe49c91431613b9442fbe84b3d156ebd285ef0289eb03cf5",
+    REFRESH_TOKEN_SECRET:"4f2663b7366418b0d14f323df3778da8cb5e84dc38e2f0b866ba9651fa16a33fef6c35c6a506091406119127b0d6932ed962fb0c31f765a0e497a8ae2a01ebdd",
+    Service_ID:"VA80291d7f287e71c1ceca362881a550e2",
+    S3_ACESS_KEY:"AKIAW5LAGONUZLTALI4V",
+    S3_SECRET_KEY:"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",
 
 
 
